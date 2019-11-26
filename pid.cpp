100f5c0d8210316f4ae4564e44bdddd8e568d299
MichaelD33 <codeing33@yahoo.com>
2019-11-26 10:42:55 -0500

    removed unnecessary axes

--- pid.cpp
+++ pid.cpp
@@ -4,12 +4,13 @@
 #include "pid.h"
 #include "config.h"
 
-float outputX, outputY, outputZ;
+
 unsigned long lastTime, currentT;
 
-axis_int16_t desiredAngle;
-axis_float_t currentAngle, lastAngle;
-axis_float_t error, deltaError, errorSum;
+int desiredAngle;
+float currentAngle, lastAngle;
+float error, deltaError, errorSum;
+float output;
 
 int_pwmOut motorSpeed;
 
@@ -18,9 +19,7 @@ void initPids(){
     
     computePids();
     resetPids();
-    lastAngle.x = currentAngle.x;
-    lastAngle.y = currentAngle.y;
-    lastAngle.z = currentAngle.z;
+    lastAngle = currentAngle;
     lastTime = currentT;
 } 
     
@@ -32,122 +31,64 @@ void computePids(){
     #endif
     
     #ifdef HORIZON
-      currentAngle.x = imu_angles().x; //read angle from IMU and set it to the current angle
-      currentAngle.y = imu_angles().y;
-      currentAngle.z = imu_angles().z;
-      //currentAngle.z = 0;
+      currentAngle = (sqrt(imu_angles().y*imu_angles().y + imu_angles().x*imu_angles().x) * tan(imu_angles().y / imu_angles().x)); //read angle from IMU and set it to the current angle
     #endif
 
     #ifdef ACRO
-      currentAngle.x = imu_rates().x; //read gyro rates from IMU and set it to the current angle
-      currentAngle.y = imu_rates().y;
-      currentAngle.z = imu_rates().z;
-      //currentAngle.z = 0;
+      currentAngle = (sqrt(imu_rates().y*imu_rates().y + imu_rates().x*imu_rates().x) * tan(imu_rates().y / imu_rates().x)); //read gyro rates from IMU and set it to the current angle
     #endif
+
+    Serial.print(currentAngle);
     
-    // compute all the working error vars
-    // read rotational rate data (°/s) from remote and set it to the desired angle
-
-
-
-    error.x = (-1 * chRoll())  - currentAngle.x;                      //present error
-    error.y = (-1 * chPitch()) - currentAngle.y;
-    error.z = chYaw()          - currentAngle.z;
-
-/*
-    if(chAux2() == 0){ 
-      error.x = 45 - currentAngle.x;    // setting a fixed error value for step response
-      error.y = currentAngle.y;
-      error.z = chYaw()- currentAngle.z;
-    }else{
-      error.x = (-1 * chRoll())  - currentAngle.x;                      
-      error.y = (-1 * chPitch()) - currentAngle.y;
-      error.z = chYaw()          - currentAngle.z; 
-    }
-*/
+    // error = (-1 * chRoll())  - currentAngle ;
+    error = 0 - currentAngle;
     
     #ifndef LOOP_SAMPLING
-    
-    long timeChange = (currentT - lastTime);
-    
-    errorSum.x += error.x * timeChange;                               //integral of error
-    errorSum.y += error.y * timeChange;
-    errorSum.z += error.z * timeChange;
-    
-    deltaError.x = (currentAngle.x - lastAngle.x) / timeChange;       //derivative of error
-    deltaError.y = (currentAngle.y - lastAngle.y) / timeChange; 
-    deltaError.z = (currentAngle.z - lastAngle.z) / timeChange; 
-   
+      long timeChange = (currentT - lastTime);
+      
+      errorSum += error * timeChange;
+      deltaError = (currentAngle - lastAngle) / timeChange;       
     #else
-    errorSum.x += error.x;                              //integral of error
-    errorSum.y += error.y;
-    errorSum.z += error.z;
-    
-    deltaError.x = currentAngle.x - lastAngle.x;        //derivative of error
-    deltaError.y = currentAngle.y - lastAngle.y; 
-    deltaError.z = currentAngle.z - lastAngle.z; 
+      errorSum += error;
+      deltaError = (currentAngle - lastAngle);
     #endif
     
     //compute integral
-    float Ix = KiX * errorSum.x;
-    float Iy = KiY * errorSum.y;
-    float Iz = KiZ * errorSum.z;
+    float integral = Ki * errorSum;
+
 
     
     //clamp the range of integral values
-    if(Ix > MAX_INTEGRAL){ 
-      Ix = MAX_INTEGRAL; 
-    }else if (Ix < (MAX_INTEGRAL * -1)){
-      Ix = MAX_INTEGRAL * -1;
-    }
-        
-    if(Iy > MAX_INTEGRAL){ 
-      Iy = MAX_INTEGRAL; 
-    }else if (Iy < (MAX_INTEGRAL * -1)){
-      Iy = MAX_INTEGRAL * -1;
-    }
-        
-    if(Iz > MAX_INTEGRAL){ 
-      Iz = MAX_INTEGRAL; 
-    }else if (Iz < (MAX_INTEGRAL * -1)){
-      Iz = MAX_INTEGRAL * -1;
+    if(integral > MAX_INTEGRAL){ 
+      integral = MAX_INTEGRAL; 
+    }else if (integral < (MAX_INTEGRAL * -1)){
+      integral = MAX_INTEGRAL * -1;
     }
 
-    outputX = (KpX * error.x + Ix - KdX * deltaError.x);
-    outputY = (KpY * error.y + Iy - KdY * deltaError.y);
-    outputZ = (KpZ * error.z + Iz - KdZ * deltaError.z);
+    output = (Kp * error + integral - Kd * deltaError);
 
 /*
     #ifdef PRINT_SERIALDATA
       if(chAux2() == 0){
         Serial.print(",");
-        Serial.print(outputX);
-        Serial.print(",");
-        Serial.print(outputY);
-        Serial.print(",");
-        Serial.print(outputZ);
+        Serial.print(output);
       }
     #endif
 
 
     //removed integral clamping
-    outputX = (KpX * error.x + KiX * errorSum.x - KdX * deltaError.x);
-    outputY = (KpY * error.y + KiY * errorSum.y - KdY * deltaError.y);
-    outputZ = (KpZ * error.z + KiZ * errorSum.z - KdZ * deltaError.z);
-
+    outputX = (Kp * error + Ki * errorSum - Kd * deltaError);
 
     //compute PID output as a function of the throttle
-    outputX = (KpX * error.x + Ix - KdX * deltaError.x)*(chThrottle() / (ESC_MAX * ESC_TOLERANCE));
-    outputY = (KpY * error.y + Iy - KdY * deltaError.y)*(chThrottle() / (ESC_MAX * ESC_TOLERANCE));
-    outputZ = (KpZ * error.z + Iz - KdZ * deltaError.z)*(chThrottle() / (ESC_MAX * ESC_TOLERANCE));
+    outputX = (Kp * error + integral - Kd * deltaError) * (chThrottle() / (ESC_MAX * ESC_TOLERANCE));
 */
  
   
     //write outputs to corresponding motors at the corresponding speed
-     motorSpeed.one = abs(chThrottle() + outputX - outputY - outputZ); 
-     motorSpeed.two = abs(chThrottle() - outputX - outputY + outputZ); 
-     motorSpeed.three = abs(chThrottle() - outputX + outputY - outputZ);
-     motorSpeed.four = abs(chThrottle() + outputX + outputY + outputZ);
+     //motorSpeed.one = abs(chThrottle() + outputX - outputY - outputZ); 
+     motorSpeed.two = abs(chThrottle() - output); 
+     //motorSpeed.three = abs(chThrottle() - outputX + outputY - outputZ);
+     motorSpeed.four = abs(chThrottle() + output);
      
      //clamp the min and max output from the pid controller (to match the needed 0-255 for pwm)
      if(motorSpeed.one > ESC_MAX){
@@ -179,15 +120,11 @@ void computePids(){
 void resetPids(){
 
    if(chThrottle() < 10){    
-      errorSum.x = 0;
-      errorSum.y = 0;
-      errorSum.z = 0;   
+      errorSum = 0;
       
     }else if(armingState() != lastArmingState()){
     //reset the integral term when the quadcopter is armed
-      errorSum.x = 0;
-      errorSum.y = 0;
-      errorSum.z = 0;
+      errorSum = 0;
     
   }
        
