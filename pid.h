5bd11ee44b29d0e5e56c233a64fd570e5ee309ff
MichaelD33 <codeing33@yahoo.com>
2019-11-26 10:43:21 -0500

    removed unnecessary axes

--- pid.h
+++ pid.h
@@ -7,30 +7,18 @@
 #include "config.h"
 
 /*  DEFINE PROPORTIONAL CONSTANTS */
-#define KpX 0.0
-#define KpY 0.0
-#define KpZ 0.00
+#define Kp 0.0
 
 #ifndef LOOP_SAMPLING
 
-  #define KiX 0.000000
-  #define KiY 0.000000
-  #define KiZ 0.0
-
-  #define KdX 0  //10
-  #define KdY 0 //10
-  #define KdZ 0  //7
+  #define Ki 0.000000
+  #define Kd 0  //10
 
 #else
 
 /*  DEFINE INTEGRAL AND DERIVATIVE CONSTANTS ACCORDING TO PID SAMPLE TIME */    
-  #define KiX 0.000000 * PID_SAMPLETIME  //0000005
-  #define KiY 0.000000 * PID_SAMPLETIME  //0000005
-  #define KiZ 0.0 * PID_SAMPLETIME  //0000005
-
-  #define KdX 3 / PID_SAMPLETIME  //10
-  #define KdY 3 / PID_SAMPLETIME  //10
-  #define KdZ 10 / PID_SAMPLETIME  //7
+  #define Ki 0.000000
+  #define Kd 0
   
 #endif
 
